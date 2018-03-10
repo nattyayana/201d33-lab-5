7ed4fa038bade505594c75b08a241fd08cc37afd
nattyayana <natnalezayana@gmail.com>
2018-03-09 19:06:22 -0800

    almost done with question 3

--- app.js
+++ app.js
@@ -49,22 +49,46 @@ testMultiply(5,9); //eslint-disable-line
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments:
+returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers, and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
+
 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
+
 // Write your code here
+//
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+
+//var sumAndMultiply = (4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.';
+
+  var sum3Numbers = sum(sum(a,b)[0],c)[0];
+  console.log(sum3Numbers);
+
+  var sum3NumString =( a + ' and ' + b + 'and ' + c + ' sum to ' + sum3Numbers + '.');
+  console.log(sum3Numbers);
+
+  var product3= multiply(multiply(a,b)[0],c);
+  console.log(product3);
+
+  var prod3StringNum =('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product3 + '.');
+
+
+  return[sum3Numbers, product3, sum3NumString, prod3StringNum];
+  //var prodSum= (a * b * c);
+
+  //var sumOfThree= [sumAdd, prodSum, '4 and 7 and 5 sum to 16', 'The product of 4 and 7 and 5 is 140.'];
 
 }
 
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
